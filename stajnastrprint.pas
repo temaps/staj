3ed8880e0fbc6dd4dca0122ea04d56b34e59a372
temaps <tema@proskurnev.name>
2016-02-01 21:43:03 +0300

    ver

---
+++ stajnastrprint.pas
@@ -0,0 +1,34 @@
+unit stajnastrprint;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, EditBtn,
+  StdCtrls, Buttons;
+
+type
+
+  { TForm3 }
+
+  TForm3 = class(TForm)
+    BitBtn1: TBitBtn;
+    CheckBox1: TCheckBox;
+    DateEdit1: TDateEdit;
+    Label1: TLabel;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
